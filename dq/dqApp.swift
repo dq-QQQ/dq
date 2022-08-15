cc018cea0636030f30a670ea73cc1678b47ecce5
SIE-KYIN <kjsl4tw@naver.com>
2022-08-15 13:38:57 +0900

    졸작 시작

---
+++ dq/dqApp.swift
@@ -0,0 +1,17 @@
+//
+//  dqApp.swift
+//  dq
+//
+//  Created by Kyu jin Lee on 2022/08/15.
+//
+
+import SwiftUI
+
+@main
+struct dqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
