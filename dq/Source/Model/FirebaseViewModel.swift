a4c3c5a078248f8da36a29b06b236d88c730dd1b
SIE-KYIN <kjsl4tw@naver.com>
2022-09-09 14:53:34 +0900

    style: 코드 리뷰/멘토링 후 리팩토링 완료

---
+++ dq/Source/Model/FirebaseViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  FirebaseViewModel.swift
+//  dq
+//
+//  Created by Kyu jin Lee on 2022/08/20.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+class FirebaseModel<T: Identifiable & Codable> {
+    let dbCollection: CollectionReference
+    
+    init(_ collectionName: String) {
+        dbCollection = Firestore.firestore().collection(collectionName)
+    }
+
+    func updateFireStore(data: T)  {
+        dbCollection.whereField("id", isEqualTo: data.id).getDocuments { snap, err in
+            if err != nil {
+                print("Error")
+                return
+            }
+            
+            for i in snap!.documents {
+                var isInterested = i.get("isInterested") as! Bool
+                isInterested.toggle()
+                DispatchQueue.main.async {
+                    i.reference.updateData(["isInterested":isInterested])
+                }
+            }
+        }
+    }
+    
+    func fetchFireStore() async -> [T] {
+        guard let snapshot = try? await dbCollection.getDocuments() else {
+            return []
+        }
+        let data: [T] = snapshot.documents.compactMap { document in
+            try? document.data(as: T.self)
+        }
+        return data
+    }
+}
--- dq/Source/ViewModel/FirebaseViewModel.swift
+++ dq/Source/ViewModel/FirebaseViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  FirebaseViewModel.swift
+//  CustomFirebase.swift
 //  dq
 //
 //  Created by Kyu jin Lee on 2022/08/20.
@@ -9,37 +9,18 @@ import Foundation
 import Firebase
 import FirebaseFirestoreSwift
 
-class FirebaseViewModel<T: Identifiable & Codable>: ObservableObject {
-    let dbCollection: CollectionReference
-    
-    init(_ collectionName: String) {
-        dbCollection = Firestore.firestore().collection(collectionName)
-    }
+class BootcampViewModel: FirebaseModel<BootcampModel>, ObservableObject {
+    @Published var bootcampList: [[BootcampModel]] = [[]]
+//    
+//    func newBootcamp() async {
+//        let tmp = BootcampModel(id: UUID().uuidString, name: "코드 스타터 캠프", process: ["iOS"], isInterested: true,applyDeadline: Timestamp(), homepage: "",period: "5주", place: ["온라인", "온라인"], fee: "55만원")
+//        _ = try? dbCollection.addDocument(from: tmp)
+//    }
+}
 
-    func updateFireStore(data: T)  {
-        dbCollection.whereField("id", isEqualTo: data.id).getDocuments { snap, err in
-            if err != nil {
-                print("Error")
-                return
-            }
-            
-            for i in snap!.documents {
-                var isInterested = i.get("isInterested") as! Bool
-                isInterested.toggle()
-                DispatchQueue.main.async {
-                    i.reference.updateData(["isInterested":isInterested])
-                }
-            }
-        }
-    }
-    
-    func fetchFireStore() async -> [T] {
-        guard let snapshot = try? await dbCollection.getDocuments() else {
-            return []
-        }
-        let data: [T] = snapshot.documents.compactMap { document in
-            try? document.data(as: T.self)
-        }
-        return data
+class ClubViewModel: FirebaseModel<ClubModel>, ObservableObject {
+    func newClub(name: String, process: [String]) async {
+        let tmp = ClubModel(id: UUID().uuidString, name: name, process: process, time: Timestamp())
+        _ = try? dbCollection.addDocument(from: tmp)
     }
 }
